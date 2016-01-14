def5747adff243cf723d3e5e859c758ec082ddf0
makenaw <makena.walsh@gmail.com>
2016-01-14 10:19:29 +0800

    adds photos controller

---
+++ favorite-place/PhotoController.swift
@@ -0,0 +1,35 @@
+//
+//  PhotoController.swift
+//  favorite-place
+//
+//  Created by makena  on 1/14/16.
+//  Copyright © 2016 makena . All rights reserved.
+//
+
+import UIKit
+
+class PhotoController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
